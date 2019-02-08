194bc0e1484a4b1209049d3dc604ed03ec307b04
Dermache Djamel <dj.der.13@gmail.com>
2019-02-08 23:14:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,35 +14,32 @@ for (var i = 0; i < containers.length; i++) {
   containers[i].addEventListener('click', (event) => {
     let flipped         = document.querySelectorAll('.flip');
 
-    if (always_flipped.length != 16) {
-      if (flipped.length > 2) {
-        unflip();
-      }
+    if (flipped.length > 2) {
+      unflip();
+    }
 
-      if (flipped.length == 2) {
-        var names = [getImgName(flipped[0]), getImgName(flipped[1])];
-        if (names[0] === names[1]) {
-          for (var j = 0; j < flipped.length; j++) {
-              flipped[j].classList.remove('flip');
-              flipped[j].classList.add('always-flipped');
-          }
-
-          write_note('Good!');
-
-        } else {
-
-          setTimeout(function(){
-            write_note('keep trying');
-            for (var j = 0; j < flipped.length; j++) {
-                flipped[j].classList.remove('flip');
-            }
-          }
-          , 1000);
+    if (flipped.length == 2) {
+      var names = [getImgName(flipped[0]), getImgName(flipped[1])];
+      if (names[0] === names[1]) {
+        for (var j = 0; j < flipped.length; j++) {
+            flipped[j].classList.remove('flip');
+            flipped[j].classList.add('always-flipped');
         }
 
-      }/* second if */
+        write_note('Good!');
 
-    }
+      } else {
+
+        setTimeout(function(){
+          write_note('keep trying');
+          for (var j = 0; j < flipped.length; j++) {
+              flipped[j].classList.remove('flip');
+          }
+        }
+        , 1000);
+      }
+
+    }/* second if */
 
 
   });
